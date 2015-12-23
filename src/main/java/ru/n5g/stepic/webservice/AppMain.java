64f967174f5467bd9b95fc952baf8e28f05d21a2
Gleb Belyaev <n5g@ya.ru>
2015-12-23 16:39:06 +0300

    add logging in Main class

--- src/main/java/ru/n5g/stepic/webservice/AppMain.java
+++ src/main/java/ru/n5g/stepic/webservice/AppMain.java
@@ -3,6 +3,8 @@ package ru.n5g.stepic.webservice;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.n5g.stepic.webservice.servlets.AllGetRequestsServlet;
 
 import static org.eclipse.jetty.servlet.ServletContextHandler.SESSIONS;
@@ -13,6 +15,8 @@ import static org.eclipse.jetty.servlet.ServletContextHandler.SESSIONS;
  * @since 22.12.15
  */
 public class AppMain {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AppMain.class);
+
     public static void main(String[] args) throws Exception {
         AllGetRequestsServlet allGetRequests = new AllGetRequestsServlet();
 
@@ -22,6 +26,7 @@ public class AppMain {
         Server server = new Server(8080);
         server.setHandler(context);
         server.start();
+        LOGGER.info("Server started");
         server.join();
     }
 }
